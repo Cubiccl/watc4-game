98153f945582869b5020c0dcf465c85c61fe542a
Cubiccl <ccl.clement@hotmail.fr>
2016-03-09 19:05:47 +0100

    Javadoc on GameState

--- src/net/watc4/game/states/GameState.java
+++ src/net/watc4/game/states/GameState.java
@@ -3,12 +3,17 @@ package net.watc4.game.states;
 import net.watc4.game.entity.EntityPlayer;
 import net.watc4.game.map.Map;
 
+/** Represents the main game engine. */
 public class GameState
 {
+	/** The Light Player. */
 	private EntityPlayer entityLumi;
+	/** The Shadow Player. */
 	private EntityPlayer entityPattou;
+	/** The world they evolve into. */
 	private Map map;
 
+	/** Creates the GameState. */
 	public GameState()
 	{
 		this.entityLumi = new EntityPlayer();
@@ -16,6 +21,7 @@ public class GameState
 		this.map = new Map();
 	}
 
+	/** @return The <code>Map</code>. */
 	public Map getMap()
 	{
 		return this.map;
