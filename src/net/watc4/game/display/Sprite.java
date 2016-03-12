92b694b111b271dfa2d8682ed94a3076a5c6beac
Cubiccl <ccl.clement@hotmail.fr>
2016-03-13 00:12:53 +0100

    Added Sprite Sheet support
    Also added Pattou's and Lumi's textures

--- src/net/watc4/game/display/Sprite.java
+++ src/net/watc4/game/display/Sprite.java
@@ -11,20 +11,48 @@ public enum Sprite
 {
 
 	TILE_DEFAULT("res/textures/tiles/default.png"),
+	TILE_GROUND("res/textures/tiles/ground.png"),
 	TILE_WALL("res/textures/tiles/wall.png"),
-	TILE_GROUND("res/textures/tiles/ground.png");
+	PATTOU_IDLE1("res/textures/playerPattou.png", 0, 0, 32),
+	PATTOU_IDLE2("res/textures/playerPattou.png", 32, 0, 32),
+	PATTOU_MOVING1("res/textures/playerPattou.png", 0, 32, 32),
+	PATTOU_MOVING2("res/textures/playerPattou.png", 0, 64, 32),
+	PATTOU_MOVING3("res/textures/playerPattou.png", 0, 96, 32),
+	PATTOU_MOVING4("res/textures/playerPattou.png", 0, 128, 32),
+	PATTOU_MOVING5("res/textures/playerPattou.png", 0, 160, 32),
+	PATTOU_MOVING6("res/textures/playerPattou.png", 0, 192, 32),
+	LUMI("res/textures/playerLumi.png", 0, 0, 32),
+	LUMI_EYE("res/textures/playerLumi.png", 0, 32, 32);
 
 	/** The actual Image. */
 	private BufferedImage image;
-	/** The URL to the Image file. */
+	/** Its size. -1 if it is the entire Sprite sheet. */
+	private int size;
+	/** The URL to the Sprite sheet file. */
 	private String url;
+	/** The position of the Sprite in the Sprite sheet. */
+	private int x, y;
 
 	/** A Sprite.
 	 * 
 	 * @param url - The path to its Image. */
 	private Sprite(String url)
 	{
+		this(url, 0, 0, -1);
+	}
+
+	/** A Sprite in a Sprite sheet.
+	 * 
+	 * @param url - The path to its Sprite sheet.
+	 * @param x - Its x position in the sheet.
+	 * @param y - Its y position in the sheet.
+	 * @param size - Its size. */
+	private Sprite(String url, int x, int y, int size)
+	{
 		this.url = url;
+		this.x = x;
+		this.y = y;
+		this.size = size;
 	}
 
 	/** @return The Image to show. */
@@ -41,6 +69,7 @@ public enum Sprite
 		try
 		{
 			this.image = ImageIO.read(new File(url));
+			if (size != -1) this.image = this.image.getSubimage(x, y, size, size);
 		} catch (IOException e)
 		{
 			System.out.println("\"" + url + "\" is not a valid File. Using default Sprite.");
