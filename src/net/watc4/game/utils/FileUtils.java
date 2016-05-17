47c48e2466afecda261796d551163c45c6c5bc55
Antoine Chédin <antoine.chedin@gmail.com>
2016-05-17 16:24:31 +0200

    New function that i often use for debug

--- src/net/watc4/game/utils/FileUtils.java
+++ src/net/watc4/game/utils/FileUtils.java
@@ -1,5 +1,7 @@
 package net.watc4.game.utils;
 
+import java.awt.Graphics2D;
+import java.awt.geom.AffineTransform;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -55,4 +57,20 @@ public class FileUtils
 	{
 		return (f - (double) ((int) f) < 0.5) ? (int) f : (int) f + 1;
 	}
+	
+	/** Draw an arrow, useful for debug */
+	void drawArrow(Graphics2D g, int x1, int y1, int x2, int y2) {
+      
+        double dx = x2 - x1, dy = y2 - y1;
+        double angle = Math.atan2(dy, dx);
+        int len = (int) Math.sqrt(dx*dx + dy*dy);
+        AffineTransform at = AffineTransform.getTranslateInstance(x1, y1);
+        at.concatenate(AffineTransform.getRotateInstance(angle));
+        g.transform(at);
+
+        // Draw horizontal arrow starting in (0, 0)
+        g.drawLine(0, 0, len, 0);
+        g.fillPolygon(new int[] {len, len-6, len-6, len},
+                      new int[] {0, -6, 6, 0}, 4);
+    }
 }
